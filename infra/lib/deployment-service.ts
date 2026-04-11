ac81dfc14751a553652d7ea440eaee2f1fb4d5c3
Pavel Yatskevich <PavelYatskevich@users.noreply.github.com>
2026-04-11 17:06:52 +0200

    feat: add scripts

--- infra/lib/deployment-service.ts
+++ infra/lib/deployment-service.ts
@@ -1,62 +1,64 @@
+import * as path from 'node:path';
 import {
-    aws_cloudfront,
-    aws_cloudfront_origins,
-    aws_s3,
-    aws_s3_deployment,
-    CfnOutput,
-    RemovalPolicy,
-  } from 'aws-cdk-lib';
-  import { Construct } from 'constructs';
-  
-  const path = './resources/build';
-  
-  export class DeploymentService extends Construct {
-    constructor(scope: Construct, id: string) {
-      super(scope, id);
-  
-      const hostingBucket = new aws_s3.Bucket(this, 'FrontendBucket', {
-        blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
-        removalPolicy: RemovalPolicy.DESTROY,
-      });
-  
-      const distribution = new aws_cloudfront.Distribution(
-        this,
-        'CloudfrontDistribution',
-        {
-          defaultBehavior: {
-            origin: aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
-              hostingBucket
-            ),
-            viewerProtocolPolicy: aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+  aws_cloudfront,
+  aws_cloudfront_origins,
+  aws_s3,
+  aws_s3_deployment,
+  CfnOutput,
+  RemovalPolicy,
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+/** Parent app build output (run `npm run build` from repository root before `cdk deploy`). */
+const appDistPath = path.resolve(__dirname, '..', '..', 'dist');
+
+export class DeploymentService extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const hostingBucket = new aws_s3.Bucket(this, 'FrontendBucket', {
+      blockPublicAccess: aws_s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const distribution = new aws_cloudfront.Distribution(
+      this,
+      'CloudfrontDistribution',
+      {
+        defaultBehavior: {
+          origin: aws_cloudfront_origins.S3BucketOrigin.withOriginAccessControl(
+            hostingBucket
+          ),
+          viewerProtocolPolicy: aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        defaultRootObject: 'index.html',
+        errorResponses: [
+          {
+            httpStatus: 404,
+            responseHttpStatus: 200,
+            responsePagePath: '/index.html',
           },
-          defaultRootObject: 'index.html',
-          errorResponses: [
-            {
-              httpStatus: 404,
-              responseHttpStatus: 200,
-              responsePagePath: '/index.html',
-            },
-          ],
-        }
-      );
-  
-      new aws_s3_deployment.BucketDeployment(this, 'BucketDeployment', {
-        sources: [aws_s3_deployment.Source.asset(path)],
-        destinationBucket: hostingBucket,
-        distribution,
-        distributionPaths: ['/*'],
-      });
-  
-      new CfnOutput(this, 'CloudFrontURL', {
-        value: distribution.domainName,
-        description: 'The distribution URL',
-        exportName: 'CloudfrontURL',
-      });
-  
-      new CfnOutput(this, 'BucketName', {
-        value: hostingBucket.bucketName,
-        description: 'The name of the S3 bucket',
-        exportName: 'BucketName',
-      });
-    }
+        ],
+      }
+    );
+
+    new aws_s3_deployment.BucketDeployment(this, 'BucketDeployment', {
+      sources: [aws_s3_deployment.Source.asset(appDistPath)],
+      destinationBucket: hostingBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    new CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.distributionDomainName,
+      description: 'The distribution URL',
+      exportName: 'CloudfrontURL',
+    });
+
+    new CfnOutput(this, 'BucketName', {
+      value: hostingBucket.bucketName,
+      description: 'The name of the S3 bucket',
+      exportName: 'BucketName',
+    });
   }
+}
